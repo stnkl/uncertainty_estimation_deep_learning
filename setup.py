962bc7f6d301fc0414463c3231de4f8f996b0827
stnkl <stain.sr@gmail.com>
2021-03-01 18:40:35 +0100

    Support installing as package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='uncertainty_estimation_deep_learning',
+      version='1.0',
+      description='A General Framework for Uncertainty Estimation in Deep Learning',
+      url='https://github.com/stnkl/uncertainty_estimation_deep_learning',
+      license='MIT',
+      packages=['contrib'],
+      zip_safe=False)
